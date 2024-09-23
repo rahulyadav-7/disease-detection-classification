87c3ab799b35f68f8aba5af31e643f7c4a104c67
rahulyadav-7 <ry7.rahulyadav@gmail.com>
2024-09-24 02:43:21 +0530

    completed till components

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_url: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+@dataclass(forzen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+@dataclass(forzen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorbaord_root_log_dir: Path
+    checkpoint_model_filepath: Path
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
+    
